d3e68dbee8fedaf80db7e7154e20cf4fcbd71950
Megan <mennis0145@gmail.com>
2019-09-24 17:35:56 +0100

    Implemented GameContainer to house components
    Co-authored-by: Seabayley <seabayley@outlook.com>

--- celebrity/src/App.js
+++ celebrity/src/App.js
@@ -1,30 +1,28 @@
-import React from 'react';
-import './App.css';
-import NavBar from './components/NavBar/NavBar.js';
-import { Switch, Route} from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar.js";
+import { Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import Start from './components/Start';
-import './App.css';
-import End from './components/End'
-
+import Start from "./components/Start";
+import "./App.css";
+import End from "./components/End";
+import GameContainer from "./components/Quiz/GameContainer";
 
 function App() {
   return (
     <div className="App">
-     <NavBar />
-    <div className="content">
-
-     
-      <Switch>
-        <Route exact path='/' component={Start} />
-        <Route path="/login" component={Login} /> 
-        <Route path="/signup" component={Signup} /> 
-        <Route exact path='/end' component={End} />
-      </Switch>
-  
+      <NavBar />
+      <div className="content">
+        <Switch>
+          <Route exact path="/" component={Start} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route exact path="/end" component={End} />
+          <Route exact path="/quiz" component={GameContainer} />
+        </Switch>
+      </div>
     </div>
-  </div>
   );
 }
 
