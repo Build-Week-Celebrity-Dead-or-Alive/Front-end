7f4d665a219d3a4c27f86008f2e7253e15398085
Megan <mennis0145@gmail.com>
2019-09-24 21:26:41 +0100

    QuizDisplay implemented

--- celebrity/src/App.js
+++ celebrity/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar.js";
@@ -9,26 +8,12 @@ import Start from "./components/Start";
 import "./App.css";
 import End from "./components/End";
 import GameContainer from "./components/Quiz/GameContainer";
-=======
-import React from 'react';
-import './App.css';
-import NavBar from './components/NavBar/NavBar.js';
-import { Switch, Route } from 'react-router-dom';
-import Login from "./components/Login";
-import Signup from "./components/Signup";
-import Start from './components/Start';
-import './App.css';
-import End from './components/End'
-import CelebrityCard from './components/Quiz/CelebrityCard';
-
->>>>>>> fa96cae469c8231df8747766cee3c129cdcee1d8
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <div className="content">
-<<<<<<< HEAD
         <Switch>
           <Route exact path="/" component={Start} />
           <Route path="/login" component={Login} />
@@ -36,18 +21,6 @@ function App() {
           <Route exact path="/end" component={End} />
           <Route exact path="/quiz" component={GameContainer} />
         </Switch>
-=======
-
-
-        <Switch>
-          <Route exact path='/' component={Start} />
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={Signup} />
-          <Route exact path='/end' component={End} />
-          <Route exact path='/quiz' component={CelebrityCard} />
-        </Switch>
-
->>>>>>> fa96cae469c8231df8747766cee3c129cdcee1d8
       </div>
     </div>
   );
--- celebrity/src/components/Quiz/QuizDisplay.jsx
+++ celebrity/src/components/Quiz/QuizDisplay.jsx
@@ -1,1 +1,92 @@
+import React, { useState } from "react";
+import styled from "styled-components";
 
+const StyledQuizDisplay = styled.div`
+  width: 30vw;
+  background-color: black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-left: 2rem;
+  .flashcard {
+    background-color: transparent;
+    width: 300px;
+    height: 200px;
+    perspective: 1000px;
+
+    .flashcard-inner {
+      position: relative;
+      width: 100%;
+      height: 100%;
+      text-align: center;
+      transition: transform 0.8s;
+      transform-style: preserve-3d;
+
+      .flashcard-front,
+      .flashcard-back {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        backface-visibility: hidden;
+        color: white;
+        font-size: 2.5rem;
+      }
+      .flashcard-back {
+        color: white;
+        transform: rotateY(180deg);
+      }
+    }
+  }
+
+  .doabuttons {
+    display: flex;
+    align-items: center;
+    color: white;
+    #dead {
+      background-color: red;
+      color: white;
+    }
+    #alive {
+      background-color: green;
+      color: white;
+    }
+  }
+`;
+
+export default function QuizDisplay({ currentCard, setCurrentCard, mockCelebList, setAnswer, answer }) {
+  
+
+  const onBtnClick = e => {
+      if(e.target.id === 'dead' && currentCard.isDead ) {
+          setAnswer('Correct');
+      } else {
+          setAnswer('Incorrect');
+      }
+      setCurrentCard(mockCelebList[currentCard.id+1])
+    document.querySelector(".flashcard-inner").style.transform =
+      "rotateY(180deg)";
+  };
+  return (
+    <StyledQuizDisplay>
+      <div className="flashcard">
+        <div className="flashcard-inner">
+          <div className="flashcard-front">
+            <h1>?</h1>
+          </div>
+          <div className="flashcard-back">
+            <h1>{answer}</h1>
+          </div>
+        </div>
+      </div>
+      <div className="doabuttons">
+        <button id="dead" onClick={onBtnClick}>
+          Dead
+        </button>
+        <p>or</p>
+        <button id="alive" onClick={onBtnClick}>
+          Alive
+        </button>
+      </div>
+    </StyledQuizDisplay>
+  );
+}
