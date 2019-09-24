a278dbda4f11220aec2a1be6abbc1284746f2013
zachary-peasley <zpman95@hotmail.com>
2019-09-24 16:30:43 -0500

    NameForm made

--- celebrity/src/components/NameForm.js
+++ celebrity/src/components/NameForm.js
@@ -14,4 +14,24 @@ const NameForm = (props) => {
         localStorage.setItem('SignUpCred', playerName)
         setPlayerName('')
       }
-    }
+
+      return (
+        <div className='name-form'>
+            <form onSubmit={(e) => handleSubmit(e)}>
+              <label htmlFor='firstname'>
+                Name
+                <input
+                  placeholder='Your name'
+                  value={playerName}
+                  name='firstname'
+                  onChange={handleChange}
+                />
+              </label>
+              <button type={'submit'}>SUBMIT</button>
+            </form>
+        </div>
+      )}
+    
+    export default NameForm
+
+    
