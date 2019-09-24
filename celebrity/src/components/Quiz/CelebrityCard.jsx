7b3c7c53564b9235c161dc18c626d6f931dbbc1e
Megan <mennis0145@gmail.com>
2019-09-24 17:43:28 +0100

    Fixed merger conflict

--- celebrity/src/components/Quiz/CelebrityCard.jsx
+++ celebrity/src/components/Quiz/CelebrityCard.jsx
@@ -4,15 +4,9 @@ function CelebrityCard({currentCard}) {
 
     return (
         <div className="celeb_wrapper">
-<<<<<<< HEAD
             <img src={currentCard.image_url} alt={`A picture of ${currentCard.name}`} />
             <h2>{currentCard.name}</h2>
             <p>{currentCard.info}</p>
-=======
-            <img src={mockCelebList[0].image_url} alt={`A picture of ${mockCelebList[0].name}`} />
-            <h2>{mockCelebList[0].name}</h2>
-            <p>{mockCelebList[0].info}</p>
->>>>>>> fa96cae469c8231df8747766cee3c129cdcee1d8
         </div>
     )
 }
