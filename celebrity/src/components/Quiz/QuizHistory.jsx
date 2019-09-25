4c0b7b886c10f88211320604b9a5c99d6760bd2f
Christopher Bailey <seabayley@outlook.com>
2019-09-25 11:26:02 -0500

    Added checkbox and X to quiz history

--- celebrity/src/components/Quiz/QuizHistory.jsx
+++ celebrity/src/components/Quiz/QuizHistory.jsx
@@ -1,4 +1,15 @@
 import React, { useState } from 'react';
+import styled from "styled-components";
+
+const StyledQuizHistory = styled.div`
+    .check-mark {
+        color: green;
+    }
+
+    .error-ex {
+        color: red;
+    }
+`;
 
 function QuizHistory(props) {
 
@@ -14,17 +25,24 @@ function QuizHistory(props) {
     }
 
     return (
-        <div className="history_wrapper">
-            <div className="score_wrapper">
-                <h2>Score</h2>
-                <h3>{getCurrentScore()}</h3>
+        <StyledQuizHistory>
+            <div className="history_wrapper">
+                <div className="score_wrapper">
+                    <h2>Score</h2>
+                    <h3>{getCurrentScore()}</h3>
+                </div>
+                <div className="history_list">
+                    {quizHistory.map(obj => {
+                        if (obj.correct) {
+                            return <p>{obj.name} <span className="check-mark">&#10004;</span></p>
+                        }
+                        else {
+                            return <p>{obj.name} <span className="error-ex">&#10006;</span></p>
+                        }
+                    })}
+                </div>
             </div>
-            <div className="history_list">
-                {quizHistory.map(obj => {
-                    return <p>{obj.name} - {obj.correct ? 'true' : 'false'}</p>
-                })}
-            </div>
-        </div>
+        </StyledQuizHistory>
     )
 }
 
