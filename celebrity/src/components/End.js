79dd103d409995649f323110ffe5225736e7a066
zachary-peasley <zpman95@hotmail.com>
2019-09-25 23:03:34 -0500

    isNameHere added

--- celebrity/src/components/End.js
+++ celebrity/src/components/End.js
@@ -9,11 +9,33 @@ const EndingScreen = () => {
 
   let score = Math.floor(correctGuesses * (5 * (correctGuesses / guess)));
 
+  if (isNaN(score)) {
+    score = 0;
+  }
+
+  let isNameHere = localStorage.getItem("SignUpCred") || "";
+
+  localStorage.setItem('GameScore', JSON.stringify(score));
+
+  if (isNameHere) {
+    let oldGames = JSON.parse(localStorage.getItem(isNameHere)) || [];
+    let newGame = { score: score};
+    oldGames.push(newGame);
+    localStorage.setItem(isNameHere, JSON.stringify(oldGames));
+  }
+
+
+
+
+
   return (
     <div className='end-screen'>
       <div className='end-content'>
-        {''}
-          <h2>Congrats{``}! </h2>
+        {isNameHere ? (
+          <h2>Congrats{` ${isNameHere}`}! </h2>
+          ) : (
+            <h2>Congrats!</h2>
+          )}
         <br />
         <h4>Correct Guesses: </h4>
         <h4>Total Gusses: </h4>
