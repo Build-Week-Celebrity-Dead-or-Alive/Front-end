e8b22a43ab1f91d4fdfb2a76aa9e9b252c52f1fe
Christopher Bailey <seabayley@outlook.com>
2019-09-26 08:11:19 -0500

    Revert "Zachary peasley"

--- celebrity/src/components/End.js
+++ celebrity/src/components/End.js
@@ -1,57 +1,29 @@
 import React from 'react'
 import Button from './Button'
-import NameForm from './NameForm';
+
 
 
 const EndingScreen = () => {
-  const correctGuesses = localStorage.getItem('CorrectGuesses');
-  const guess = localStorage.getItem("TotalGuesses");
-
-  let score = Math.floor(correctGuesses * (5 * (correctGuesses / guess)));
-
-  if (isNaN(score)) {
-    score = 0;
-  }
-
-  let isNameHere = localStorage.getItem("SignUpCred") || "";
-
-  localStorage.setItem('GameScore', JSON.stringify(score));
-
-  if (isNameHere) {
-    let oldGames = JSON.parse(localStorage.getItem(isNameHere)) || [];
-    let newGame = { score: score};
-    oldGames.push(newGame);
-    localStorage.setItem(isNameHere, JSON.stringify(oldGames));
-  }
-
-
+//   const correctGuesses = localStorage.getItem('CorrectGuesses');
+//   const guess = localStorage.getItem("TotalGuesses");
 
 
 
   return (
     <div className='end-screen'>
       <div className='end-content'>
-        {isNameHere ? (
-          <h2>Congrats{` ${isNameHere}`}! </h2>
-          ) : (
-            <h2>Congrats!</h2>
-          )}
+        {''}
+          <h2>Congrats{``}! </h2>
         <br />
-        <h3>You've completed the quiz!</h3>
-        <br />
-        <h4>Correct Guesses: {correctGuesses} </h4>
-        <h4>Total Gusses: {guess} </h4>
-        <h3>Score:{isNaN(score) ? 0 : score} </h3>
-        {isNameHere ? null : (  
-      <div>
-      <h4>Enter your name to save your score:</h4>
-      <NameForm score={score}/>
+        <h4>Correct Guesses: </h4>
+        <h4>Total Gusses: </h4>
+        <h3>Score: </h3>
+        {''} 
+     
+      </div>
+      <Button buttonText={'PLAY AGAIN'} pathName={'play'} />
     </div>
-  )}
-</div>
-<Button buttonText={'PLAY AGAIN'} pathName={'play'} />
-</div>
-)
+  )
 }
 
 export default EndingScreen
--- celebrity/src/components/Login.js
+++ celebrity/src/components/Login.js
@@ -1,68 +1,25 @@
 import React, { useState } from 'react'
 import Button from './Button'
-import axios from 'axios';
-import styled from 'styled-components'
-// import {Form} from '../styled-components/LoginStyle'
 
-
-export const ErrorParagraph = styled.p`
-    padding: 16px;
-    background-color: #ffb7b7;
-    color: red;
-    border: solid 1px #f39595;
-    border-radius: 4px;
-    max-width: 700px;
-    margin: 16px auto;
-`;
-
-
-const LoginScreen = (props) => {
-  const [errorMessage, setErrorMessage] = useState('');
-  const [loginCred, setloginCred] = useState({ 
-    Name: '',
-    password: '' 
-  })
-
-
-
-  const login = e => {
-    e.preventDefault();
-    axios
-        .post('https://celebs-dead-or-alive.herokuapp.com/auth/login', loginCred)
-        .then(res => {
-            console.log(res)
-            localStorage.setItem('token', res.data.token)
-            setErrorMessage('')
-            props.history.push('/add_item')
-        })
-        .catch(err => {
-            console.log(typeof err.message)
-            setErrorMessage(err.message)
-        })
-}
-
-
+const LoginScreen = () => {
+  const [loginCred, setloginCred] = useState({ email: '', password: '' })
 
   const handleChange = (e) => {
 
     setloginCred({ ...loginCred, [e.target.name]: e.target.value })
   }
 
-  window.localStorage.setItem("LoginCred",loginCred.username, JSON.stringify(loginCred))
+  window.localStorage.setItem("LoginCred", JSON.stringify(loginCred))
 
   return (
-
-    // <Form>
-    <div className='signup-screen'>
-        <h1>Dead<br /> Or
-        <br /> Alive</h1>
-      <form onSubmit={login}>
-        <label htmlFor='Name'>
-          Name
+    <div className='login-screen'>
+      <form>
+        <label htmlFor='email'>
+          Email
           <input
-            placeholder='Name'
-            value={loginCred.name}
-            name='name'
+            placeholder='email'
+            value={loginCred.email}
+            name='email'
             onChange={handleChange}
           />
         </label>
@@ -78,14 +35,10 @@ const LoginScreen = (props) => {
         </label>
         <div className='buttons'>
           <Button type='submit' buttonText={'SUBMIT'} pathName={''} />
-          {/* <Button type='submit' buttonText={'SIGNUP'} pathName={'signup'} /> */}
+          <Button type='submit' buttonText={'SIGNUP'} pathName={'signup'} />
         </div>
       </form>
-      {errorMessage && <ErrorParagraph>
-                                    {errorMessage}
-                            </ErrorParagraph>}
     </div>
-    // </Form>
   )
 }
 
